b9a9cff384e425ff1d77445b7e1c66e11162b219
Connor Carpenter <connorcarpenter@gmail.com>
2024-01-11 15:17:30 -0600

    refactor multi-client app to reduce some code reuse

--- multi-client-socket/client/app/src/app.rs
+++ multi-client-socket/client/app/src/app.rs
@@ -15,18 +15,82 @@ use naia_shared::Timer;
 use multi_client_socket_server_a_protocol::{shared_config_a, PING_MSG_A, PONG_MSG_A};
 use multi_client_socket_server_b_protocol::{shared_config_b, PING_MSG_B, PONG_MSG_B};
 
-pub struct App {
-    packet_sender_a: Box<dyn PacketSender>,
-    packet_receiver_a: Box<dyn PacketReceiver>,
-    message_count_a: u8,
-    timer_a: Timer,
-    server_addr_str_a: Option<String>,
+pub struct Client {
+    letter: String,
+    ping_msg: String,
+    pong_msg: String,
+    packet_sender: Box<dyn PacketSender>,
+    packet_receiver: Box<dyn PacketReceiver>,
+    message_count: u8,
+    timer: Timer,
+    server_addr_str: Option<String>,
+}
 
-    packet_sender_b: Box<dyn PacketSender>,
-    packet_receiver_b: Box<dyn PacketReceiver>,
-    message_count_b: u8,
-    timer_b: Timer,
-    server_addr_str_b: Option<String>,
+impl Client {
+    pub fn update(&mut self) -> bool {
+
+        if self.server_addr_str.is_none() {
+            if let ServerAddr::Found(addr) = self.packet_receiver.server_addr() {
+                self.server_addr_str = Some(addr.to_string());
+            }
+        }
+
+        match self.packet_receiver.receive() {
+            Ok(Some(packet)) => {
+                let message_from_server = String::from_utf8_lossy(packet);
+
+                info!(
+                    "Client recv <- {} ({}): {}",
+                    self.server_addr_str.as_ref().unwrap_or(&"".to_string()),
+                    self.letter,
+                    message_from_server
+                );
+
+                if message_from_server.eq(self.pong_msg.as_str()) {
+                    self.message_count += 1;
+
+                    if self.message_count == 3 {
+                        info!("Client finished sending messages to {}", self.letter);
+                        return true;
+                    }
+                }
+            }
+            Ok(None) => {
+                if self.message_count < 3 {
+                    if self.timer.ringing() {
+                        self.timer.reset();
+
+                        let message_to_server: String = self.ping_msg.clone();
+
+                        let server_addr = match self.packet_receiver.server_addr() {
+                            ServerAddr::Found(addr) => addr.to_string(),
+                            _ => "".to_string(),
+                        };
+                        info!("Client send -> {} ({}): {}", server_addr, self.letter, message_to_server);
+
+                        match self.packet_sender.send(message_to_server.as_bytes()) {
+                            Ok(()) => {}
+                            Err(error) => {
+                                info!("Client Send Error (to {}): {}", self.letter, error);
+                            }
+                        }
+                    }
+                } else {
+                    return true;
+                }
+            }
+            Err(err) => {
+                info!("Client Error (from {}): {}", self.letter, err);
+            }
+        }
+
+        return false;
+    }
+}
+
+pub struct App {
+    client_a: Option<Client>,
+    client_b: Option<Client>,
 }
 
 impl App {
@@ -36,127 +100,48 @@ impl App {
         let (packet_sender_a, packet_receiver_a) =
             Socket::connect("http://127.0.0.1:14191", &shared_config_a());
 
+        let client_a = Client {
+            letter: "A".to_string(),
+            ping_msg: PING_MSG_A.to_string(),
+            pong_msg: PONG_MSG_A.to_string(),
+            packet_sender: packet_sender_a,
+            packet_receiver: packet_receiver_a,
+            message_count: 0,
+            timer: Timer::new(Duration::from_secs(1)),
+            server_addr_str: None,
+        };
+
         let (packet_sender_b, packet_receiver_b) =
             Socket::connect("http://127.0.0.1:14193", &shared_config_b());
 
-        App {
-            packet_sender_a,
-            packet_receiver_a,
-            message_count_a: 0,
-            timer_a: Timer::new(Duration::from_secs(1)),
-            server_addr_str_a: None,
+        let client_b = Client {
+            letter: "B".to_string(),
+            ping_msg: PING_MSG_B.to_string(),
+            pong_msg: PONG_MSG_B.to_string(),
+            packet_sender: packet_sender_b,
+            packet_receiver: packet_receiver_b,
+            message_count: 0,
+            timer: Timer::new(Duration::from_secs(1)),
+            server_addr_str: None,
+        };
 
-            packet_sender_b,
-            packet_receiver_b,
-            message_count_b: 0,
-            timer_b: Timer::new(Duration::from_secs(1)),
-            server_addr_str_b: None,
+        App {
+            client_a: Some(client_a),
+            client_b: Some(client_b),
         }
     }
 
     pub fn update(&mut self) {
-
-        // Server A
-        if self.server_addr_str_a.is_none() {
-            if let ServerAddr::Found(addr) = self.packet_receiver_a.server_addr() {
-                self.server_addr_str_a = Some(addr.to_string());
+        if let Some(client_a) = &mut self.client_a {
+            if client_a.update() {
+                self.client_a = None;
+                info!("Closed Client A");
             }
         }
-
-        match self.packet_receiver_a.receive() {
-            Ok(Some(packet)) => {
-                let message_from_server = String::from_utf8_lossy(packet);
-
-                info!(
-                    "Client recv <- {} (A): {}",
-                    self.server_addr_str_a.as_ref().unwrap_or(&"".to_string()),
-                    message_from_server
-                );
-
-                if message_from_server.eq(PONG_MSG_A) {
-                    self.message_count_a += 1;
-
-                    if self.message_count_a == 10 {
-                        info!("Client finished sending messages to A");
-                    }
-                }
-            }
-            Ok(None) => {
-                if self.message_count_a < 10 {
-                    if self.timer_a.ringing() {
-                        self.timer_a.reset();
-
-                        let message_to_server: String = PING_MSG_A.to_string();
-
-                        let server_addr = match self.packet_receiver_a.server_addr() {
-                            ServerAddr::Found(addr) => addr.to_string(),
-                            _ => "".to_string(),
-                        };
-                        info!("Client send -> {} (A): {}", server_addr, message_to_server);
-
-                        match self.packet_sender_a.send(message_to_server.as_bytes()) {
-                            Ok(()) => {}
-                            Err(error) => {
-                                info!("Client Send Error (to A): {}", error);
-                            }
-                        }
-                    }
-                }
-            }
-            Err(err) => {
-                info!("Client Error (from A): {}", err);
-            }
-        }
-
-        // Server B
-        if self.server_addr_str_b.is_none() {
-            if let ServerAddr::Found(addr) = self.packet_receiver_b.server_addr() {
-                self.server_addr_str_b = Some(addr.to_string());
-            }
-        }
-
-        match self.packet_receiver_b.receive() {
-            Ok(Some(packet)) => {
-                let message_from_server = String::from_utf8_lossy(packet);
-
-                info!(
-                    "Client recv <- {} (B): {}",
-                    self.server_addr_str_b.as_ref().unwrap_or(&"".to_string()),
-                    message_from_server
-                );
-
-                if message_from_server.eq(PONG_MSG_B) {
-                    self.message_count_b += 1;
-
-                    if self.message_count_b == 10 {
-                        info!("Client finished sending messages to B");
-                    }
-                }
-            }
-            Ok(None) => {
-                if self.message_count_b < 10 {
-                    if self.timer_b.ringing() {
-                        self.timer_b.reset();
-
-                        let message_to_server: String = PING_MSG_B.to_string();
-
-                        let server_addr = match self.packet_receiver_b.server_addr() {
-                            ServerAddr::Found(addr) => addr.to_string(),
-                            _ => "".to_string(),
-                        };
-                        info!("Client send -> {} (B): {}", server_addr, message_to_server);
-
-                        match self.packet_sender_b.send(message_to_server.as_bytes()) {
-                            Ok(()) => {}
-                            Err(error) => {
-                                info!("Client Send Error (to B): {}", error);
-                            }
-                        }
-                    }
-                }
-            }
-            Err(err) => {
-                info!("Client Error (from B): {}", err);
+        if let Some(client_b) = &mut self.client_b {
+            if client_b.update() {
+                self.client_b = None;
+                info!("Closed Client B");
             }
         }
     }
